2452023f6278a24853ba4d8a9fd6c67d41d00d5c
Wyatt Entrekin <wmentrekin@gmail.com>
2025-06-30 23:55:33 -0400

    Added separate file for prior ratings, fixed week-by-week record, and added priors for new fbs teams

--- model/data.py
+++ model/data.py
@@ -3,17 +3,9 @@ import pandas as pd # type: ignore
 from dotenv import load_dotenv # type: ignore
 import os
 
-BASE_URL = "https://api.collegefootballdata.com"
-# API_KEY = "sz55AwUuro/KMnA8lmg3rzeoEa7LsKLJDZQpDKNpkDqhJF26ErBICm4GJGEyMJku"
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-
-## IN PROGRESS ##
-def get_fbs_records_by_team_by_year(year, team):
-    api_key = API_KEY
-    url = f"{BASE_URL}/teams/records?year={year}&team={team}"
-    headers = {"Authorization": f"Bearer {api_key}"}
-## IN PROGRESS ##
+BASE_URL = "https://api.collegefootballdata.com"
 
 def get_fbs_teams_by_year(year):
 
@@ -52,7 +44,7 @@ def process_game_data(games_df, fbs_teams_df):
 
     records = {}
     for team in teams:
-        records[team] = [0, 0]
+        records[team] = [0, 0, 0]
 
     for _, row in games_df.iterrows():
         team1, team2 = row['homeTeam'], row['awayTeam']
@@ -70,9 +62,37 @@ def process_game_data(games_df, fbs_teams_df):
         team2_is_fbs = team2 in teams
 
         margin = abs(team1_score - team2_score)
-        winner = team1 if team1_score > team2_score else team2
+        if team1_score > team2_score:
+            winner = team1
+        elif team2_score > team1_score:
+            winner = team2
+        else:
+            winner = "tie"
+
         location = "neutral" if row.get("neutralSite", False) else "home"
         alpha = 1 if location == "neutral" else (0.8 if row['homeTeam'] == winner else 1.2)
+
+        if team1_is_fbs and team2_is_fbs and team1 == winner:
+            records[team1][0] += 1
+            records[team2][1] += 1
+        elif team1_is_fbs and team2_is_fbs and team2 == winner:
+            records[team1][1] += 1
+            records[team2][0] += 1
+        elif team1_is_fbs and team2_is_fbs and winner == "tie":
+            records[team1][2] += 1
+            records[team2][2] += 1
+        elif team1_is_fbs and  not team2_is_fbs and team1 == winner:
+            records[team1][0] += 1
+        elif team1_is_fbs and not team2_is_fbs and team2 == winner:
+            records[team1][1] += 1
+        elif team1_is_fbs and not team2_is_fbs and winner == "tie":
+            records[team1][2] += 1
+        elif not team1_is_fbs and team2_is_fbs and team1 == winner:
+            records[team2][1] += 1
+        elif not team1_is_fbs and team2_is_fbs and team2 == winner:
+            records[team2][0] += 1
+        elif not team1_is_fbs and team2_is_fbs and winner == "tie":
+            records[team2][2] += 1
 
         # Track FCS losses
         if team1_is_fbs and not team2_is_fbs and team1_score < team2_score:
@@ -80,12 +100,6 @@ def process_game_data(games_df, fbs_teams_df):
         elif team2_is_fbs and not team1_is_fbs and team2_score < team1_score:
             fcs_losses.append((team2, margin, alpha, row.get("week"), row.get("season")))
         elif team1_is_fbs and team2_is_fbs:
-            if team1 == winner:
-                records[team1][0] += 1
-                records[team2][1] += 1
-            elif team2 == winner:
-                records[team1][1] += 1
-                records[team2][0] += 1
             games.append((
                 team1,
                 team2,
@@ -97,4 +111,12 @@ def process_game_data(games_df, fbs_teams_df):
                 row.get("season")
             ))
 
+    return teams, games, fcs_losses, records
+
+def get_data_by_year_up_to_week(year, week = None):
+    games_df = get_games_by_year_week(year)
+    if week:
+        games_df = games_df[games_df["week"] <= week]
+    fbs_teams_df = get_fbs_teams_by_year(year)
+    teams, games, fcs_losses, records = process_game_data(games_df, fbs_teams_df)
     return teams, games, fcs_losses, records
--- model/prior_model.py
+++ model/prior_model.py
@@ -1,4 +1,3 @@
-
 import cvxpy as cp # type: ignore
 import networkx as nx # type: ignore
 import numpy as np # type: ignore
@@ -7,44 +6,30 @@ from data import get_fbs_teams_by_year
 from data import get_games_by_year_week
 from data import process_game_data
 from data import get_data_by_year_up_to_week
-from prior_model import get_prior_ratings
 
-def get_ratings(year, week):
+def get_prior_ratings(year):
 
     # Get Data
-    teams, games, fcs_losses = get_data_by_year_up_to_week(year)
-    prior_ratings = get_prior_ratings(year - 1)
+    teams, games, fcs_losses, records = get_data_by_year_up_to_week(year)
 
     # Datasets
     teams = teams  # List of FBS team names
-    games = games  # List of tuples: (i, j, k, winner, margin, location_multiplier)
-    fcs_losses = fcs_losses  # List of tuples: (team, margin, location_multiplier)
-    prior_ratings = prior_ratings  # {team: prior_rating}
-
-    # Assign Prior Rating to New FBS Teams
-    for team in teams:
-        if team not in prior_ratings.keys():
-            prior_ratings[team] = 35
+    games = games  # List of tuples: (i, j, k, winner, margin, location_multiplier, week, season)
+    fcs_losses = fcs_losses  # List of tuples: (team, margin, location_multiplier, week, season)
 
     # Parameters
-    lambda_max = 10 # max prior rating multiplier
-    _lambda = cp.Parameter(nonneg=True) # calculate term based on lambda_max and connectivity matrix
     M = 200 # Big M 200 > 138 = Number of FBS teams
     mu = 20 # regularization penalty for on FCS rating
     beta = 2.0 # penalty multipler for FCS loss slack
     R_min = 5 # mininum FCS rating
     R_max = 15 # maximum FCS rating
     gamma = 1 # small regularization constant
-    R_mean = 50 # center point of ratings
 
     # Decision Variables
     r = {team: cp.Variable(name = f"r_{team}") for team in teams} # team rating
     z = {(i, j, k): cp.Variable(nonneg=True, name = f"z_{i},{j}^{k}") for (i, j, k, _, _, _, _, _) in games} # slack variable
     r_fcs = cp.Variable(name = "r_fcs") # rating for dummy FCS team
     z_fcs = {team: cp.Variable(nonneg=True, name = "z_fcs_team") for (team, _, _, _, _) in fcs_losses} # slack variable for loss to dummy FCS team
-
-    # Prior Rating Terms
-    prior_term = _lambda * cp.sum([(r[i] - prior_ratings[i])**2 for i in teams])
 
     # Slack Terms
     slack_terms = []
@@ -66,7 +51,7 @@ def get_ratings(year, week):
     ])
 
     # Objective Function
-    objective = cp.Minimize(cp.sum(slack_terms) + gamma * soft_margin_penalty + fcs_slack + fcs_reg + prior_term)
+    objective = cp.Minimize(cp.sum(slack_terms) + gamma * soft_margin_penalty + fcs_slack + fcs_reg)
 
     # Constraints
     constraints = []
@@ -74,6 +59,8 @@ def get_ratings(year, week):
         constraints.append(r[i] + z[(i, j, k)] <= r[j] + M) # slack constraints for losses to lower ranked teams
     for (team, _, _, _, _) in fcs_losses:
         constraints.append(r[team] + z_fcs[team] <= r_fcs + M) # slack constraints for losses to FCS teams
+    for (team) in teams:
+        constraints.append(r[team] >= 0)
     constraints.append(r_fcs >= R_min)
     constraints.append(r_fcs <= R_max)
 
@@ -83,13 +70,21 @@ def get_ratings(year, week):
     if problem.status not in ["infeasible", "unbounded"]:
         # Otherwise, problem.value is inf or -inf, respectively.
         print("Optimal value: %s" % problem.value)
-        ratings = []
+        prior_ratings = {}
+        slack = []
         for variable in problem.variables():
-            print("Variable %s: value %s" % (variable.name(), variable.value))
             if variable.name()[0:2] == "r_":
-                ratings.append((variable.name()[2:], variable.value))
-        ratings = sorted(ratings, key = lambda x: x[1], reverse = True)
+                prior_ratings[variable.name()[2:]] = variable.value.astype(float)
+            elif variable.name()[0:2] == "z_":
+                slack.append((variable.name()[2:], variable.value.astype(float)))
+                
+        prior_ratings = dict(sorted(prior_ratings.items(), key = lambda item: item[1], reverse = True))
         count = 1
-        for team in ratings:
-            print(count, team)
+        for team in prior_ratings.keys():
+            if team == "fcs":
+                print(count, team, prior_ratings[team])
+            else:
+                print(count, team, prior_ratings[team], f"{records[team][0]}-{records[team][1]}")
             count += 1
+
+    return prior_ratings
